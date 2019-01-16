200d3d2f3ab20d2b2423af7e8f7a385d49b4ed60
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-01-16 20:47:15 +0100

    Real player object that can move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) {
+            cam.enabled = false;
+            return;
+        }
+
+        transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * speed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
