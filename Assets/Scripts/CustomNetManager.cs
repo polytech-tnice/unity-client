b9fb622aebd1a60a74f5bbb71e824ea6452b98ae
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-01-17 16:58:12 +0100

    Authentication

--- Assets/Scripts/CustomNetManager.cs
+++ Assets/Scripts/CustomNetManager.cs
@@ -8,9 +8,23 @@ using SocketIO;
 public class CustomNetManager : NetworkManager
 {
     [SerializeField]
+    private GameObject socketIOPrefab;
+
     private SocketIOComponent socket;
+    private bool socketIOInstantiated = false;
 
     public void InitGameServer() {
+        if (!socketIOInstantiated) {
+            GameObject socketIO = Instantiate(socketIOPrefab);
+            DontDestroyOnLoad(socketIO);
+            socket = socketIO.GetComponent<SocketIOComponent>();
+            socketIOInstantiated = true;
+
+            socket.On("connect", (SocketIOEvent e) => {
+                Debug.Log("Connected to Node server!");
+                AuthenticateServer();
+            });
+        }
         StartServer();
     }
 
@@ -18,5 +32,11 @@ public class CustomNetManager : NetworkManager
         networkAddress = serverIpField.text;
 
         StartClient();
+    }
+
+    void AuthenticateServer() {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["name"] = "game";
+        socket.Emit("authentication", new JSONObject(data));
     }
 }
