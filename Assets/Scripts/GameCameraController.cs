8c20d767195654990b1acb6e031e136fda26b470
Antoine Steyer <a.steyer@radioactivit.com>
2019-02-01 15:57:37 +0100

    Add cameras to GameScene

--- Assets/Scripts/GameCameraController.cs
+++ Assets/Scripts/GameCameraController.cs
@@ -26,10 +26,16 @@ public class GameCameraController : NetworkBehaviour
   public float lStickV;
   public float lStickH;
 
+  private GameObject[] cameras;
+
+  private int currentCamera;
+
   // Start is called before the first frame update
   void Start()
   {
     cam = GetComponent<Camera>();
+    currentCamera = -1;
+    cameras = GameObject.FindGameObjectsWithTag("FixedCamera");
     body = GetComponent<Rigidbody>();
   }
 
@@ -40,6 +46,26 @@ public class GameCameraController : NetworkBehaviour
     if (!isLocalPlayer)
     {
       cam.enabled = false;
+      return;
+    }
+
+
+
+
+    if (Input.GetKeyDown(KeyCode.Joystick1Button8))
+    {
+      switchCameraRight();
+    }
+
+    if (Input.GetKeyDown(KeyCode.Joystick1Button7))
+    {
+      switchCameraLeft();
+    }
+
+
+    // If not Main Camera, don't move or rotate
+    if (currentCamera != -1)
+    {
       return;
     }
 
@@ -64,6 +90,59 @@ public class GameCameraController : NetworkBehaviour
 
     var direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
     transform.Translate(direction * speed * Time.deltaTime, Space.Self);
+  }
+
+  private void showInputs()
+  {
+    System.Array values = System.Enum.GetValues(typeof(KeyCode));
+    foreach (KeyCode code in values)
+    {
+      if (Input.GetKeyDown(code)) { print(System.Enum.GetName(typeof(KeyCode), code)); }
+    }
+  }
+
+  private void switchCameraRight()
+  {
+    if (currentCamera == -1)
+    {
+      cam.enabled = false;
+      cameras[0].GetComponent<Camera>().enabled = true;
+      currentCamera = 0;
+      return;
+    }
+
+    if (currentCamera == cameras.Length - 1)
+    {
+      cam.enabled = true;
+      cameras[cameras.Length - 1].GetComponent<Camera>().enabled = false;
+      currentCamera = -1;
+      return;
+    }
+
+    cameras[currentCamera].GetComponent<Camera>().enabled = false;
+    cameras[++currentCamera].GetComponent<Camera>().enabled = true;
+  }
+
+  private void switchCameraLeft()
+  {
+    if (currentCamera == -1)
+    {
+      cam.enabled = false;
+      cameras[cameras.Length - 1].GetComponent<Camera>().enabled = true;
+      currentCamera = cameras.Length - 1;
+      return;
+    }
+
+    if (currentCamera == 0)
+    {
+      cam.enabled = true;
+      cameras[0].GetComponent<Camera>().enabled = false;
+      currentCamera = -1;
+      return;
+    }
+
+    cameras[currentCamera].GetComponent<Camera>().enabled = false;
+    cameras[--currentCamera].GetComponent<Camera>().enabled = true;
   }
 
 }
