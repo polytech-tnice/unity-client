3d73ffb0abcdd970e3af297890540fdbaa784057
dq610272 <quentin.duret@etu.unice.fr>
2019-02-08 09:09:19 +0100

    script controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Controller : MonoBehaviour
+using UnityEngine.Networking;
+public class Controller : NetworkBehaviour
 {
 
-   void Start () {
-    Input.gyro.enabled = true;
-}
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
 
-// Update is called once per frame
-void Update () {
-    transform.Rotate (0, -Input.gyro.rotationRateUnbiased.y, 0);
-    transform.Rotate (-Input.gyro.rotationRateUnbiased.z,0 , 0);
-    transform.Rotate (0,0 , -Input.gyro.rotationRateUnbiased.x);
-     transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);     
-}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        transform.Rotate(-Input.gyro.rotationRateUnbiased.z, 0, 0);
+        transform.Rotate(0, 0, -Input.gyro.rotationRateUnbiased.x);
+        transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+    }
 }
