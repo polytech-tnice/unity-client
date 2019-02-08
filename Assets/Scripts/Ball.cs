097c6bfda148940cc6bf68a5b0272da22af0b309
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-02-08 16:14:21 +0100

    Basic ball collision

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Ball : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.CompareTag("Player")) {
+            Debug.Log("Player");
+        }
+    }
+}
