2f949ad476eb28c4a031c6073763579a0e6c260a
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-02-09 11:10:52 +0100

    Detects collisions on tennis court by zones

---
+++ Assets/Scripts/TennisCourtZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TennisCourtZone : MonoBehaviour
+{
+    public enum ZoneType {
+        IN,
+        OUT
+    }
+
+    [Range(0,1)]
+    [SerializeField]
+    private int player;
+
+    [SerializeField]
+    private ZoneType type;
+
+    void OnTriggerEnter(Collider col) {
+        Debug.Log(type.ToString() + ' ' + player);
+    }
+
+}
