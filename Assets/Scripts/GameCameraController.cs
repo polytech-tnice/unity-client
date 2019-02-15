c78ca80ed30fdaafcb605086b6e5513a1763e497
Antoine Steyer <a.steyer@radioactivit.com>
2019-02-15 08:25:32 +0100

    Update scoreboard with score

--- Assets/Scripts/GameCameraController.cs
+++ Assets/Scripts/GameCameraController.cs
@@ -15,16 +15,7 @@ public class GameCameraController : NetworkBehaviour
   public float lookSmoothDamp = 0.1f;
   public float xRotation;
   public float yRotation;
-  public float xRotationV;
-  public float yRotationV;
-  public float currentXRotation;
-  public float currentYRotation;
   [HideInInspector]
-  public float lStickHorizontal;
-  [HideInInspector]
-  public float lStickVertical;
-  public float lStickV;
-  public float lStickH;
 
   private GameObject[] cameras;
 
@@ -37,6 +28,7 @@ public class GameCameraController : NetworkBehaviour
     currentCamera = -1;
     cameras = GameObject.FindGameObjectsWithTag("FixedCamera");
     body = GetComponent<Rigidbody>();
+    transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
   }
 
 
@@ -73,19 +65,10 @@ public class GameCameraController : NetworkBehaviour
       return;
     }
 
-    lStickHorizontal = Input.GetAxis("LookY");
-    lStickVertical = Input.GetAxis("LookX");
-
-    lStickH = lStickHorizontal;
-    lStickV = lStickVertical;
-
     xRotation += Input.GetAxis("LookX") * lookSenitivity;
     yRotation += Input.GetAxis("LookY") * lookSenitivity;
 
     xRotation = Mathf.Clamp(xRotation, -90, 90);
-
-    currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothDamp);
-    currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothDamp);
 
     transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
 
