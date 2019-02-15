88f0cf32b8a5657103bbf654b756213391de385f
Antoine Steyer <a.steyer@radioactivit.com>
2019-02-15 08:58:15 +0100

    Fix updateScore

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Score : MonoBehaviour
+public class Score : NetworkBehaviour
 {
-    private int[] score = new int[2];
+  private int[] score = new int[2];
 
-    public int[] AddScore(int player, int points) {
-        if (player < 0 || player >= 2) {
-            Debug.LogError("Cannot add score to player " + player + ": not found.");
-            return score;
-        }
-
-        score[player] += points;
-        return score;
+  public int[] AddScore(int player, int points)
+  {
+    if (player < 0 || player >= 2)
+    {
+      Debug.LogError("Cannot add score to player " + player + ": not found.");
+      return score;
     }
 
-    public int[] IncrementScore(int player) {
-        return AddScore(player, 1);
-    }
+    score[player] += points;
+    return score;
+  }
 
-    public int[] GetCurrentScore() {
-        return score;
-    }
+  public int[] IncrementScore(int player)
+  {
+    int[] newScore = AddScore(player, 1);
+    RpcUpdateScore(newScore[0], newScore[1]);
+    return newScore;
+  }
+
+  public int[] GetCurrentScore()
+  {
+    return score;
+  }
+
+  [ClientRpc]
+  public void RpcUpdateScore(int score1, int score2)
+  {
+    score[0] = score1;
+    score[1] = score2;
+  }
 }
