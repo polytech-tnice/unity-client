c17071021ddc98b368667df169848100143b33ab
Julien Lemaire <julien.lemaire@sticmac.fr>
2019-02-19 21:36:01 +0100

    Fix second player racket orientation

--- Assets/Scripts/OVRRacket.cs
+++ Assets/Scripts/OVRRacket.cs
@@ -16,7 +16,7 @@ public class OVRRacket : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initRotation = transform.rotation;
+        initRotation = transform.localRotation;
     }
 
     // Update is called once per frame
